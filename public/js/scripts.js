e181eb3fa43108411b616051e57059294322bd34
willo <153154914+willoo0@users.noreply.github.com>
2024-12-20 23:02:40 -0600

    Update and rename js/scripts.js to public/js/scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -6,22 +6,20 @@ function openAboutBlank() {
   newTab.document.close();
 }
 function showGame(gameUrl, gameTitle) {
-  // Hide game selection screen
+  
   document.getElementById('game-container').style.display = 'none';
 
-  // Show the game display
   const gameFrame = document.getElementById('game-frame');
   gameFrame.src = gameUrl;
 
   document.getElementById('game-display').style.display = 'block';
-  document.getElementById('game-frame-container').style.maxWidth = '800px'; // Adjust to prevent the game from being too large
+  document.getElementById('game-frame-container').style.maxWidth = '800px'; 
 }
 
 function exitGame() {
-  // Hide the game display
+
   document.getElementById('game-display').style.display = 'none';
 
-  // Show the game selection screen again
   document.getElementById('game-container').style.display = 'flex';
 }
 
