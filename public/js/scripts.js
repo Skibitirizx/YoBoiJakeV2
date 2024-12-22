705b6f0ccb6e970207601237eb95ff47563dffd5
Skibitirizx <jacobcox6689@gmail.com>
2024-12-21 23:36:19 -0500

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,10 +1,32 @@
-function openAboutBlank() {
-  const content = document.documentElement.outerHTML;
-  const newTab = window.open('about:blank', '_blank');
-  newTab.document.open();
-  newTab.document.write(content);
-  newTab.document.close();
-}
+  var url = "/";
+  var win;
+
+  function openAboutBlank() {
+      if (win) {
+          win.focus();
+      } else {
+          win = window.open('', '_blank');
+          win.document.body.style.margin = '0';
+          win.document.body.style.height = '100vh';
+          var iframe = win.document.createElement('iframe');
+          iframe.style.border = 'none';
+          iframe.style.width = '100%';
+          iframe.style.height = '100%';
+          iframe.src = url;
+          win.document.body.appendChild(iframe);
+      }
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+      var switchInput = document.querySelector('about-blank-btn');
+
+      switchInput.addEventListener('change', function() {
+          if (this.checked) {
+              openAboutBlank();
+          }
+      });
+  });
+
 function showGame(gameUrl, gameTitle) {
   
   document.getElementById('game-container').style.display = 'none';
